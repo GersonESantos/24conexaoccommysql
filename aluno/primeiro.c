4095a72dc46d702ada0785b64bab7b7c55dd0a1e
GersonESS <gebhsantos@gmail.com>
2026-02-10 20:20:51 -0300

    Update primeiro.c

--- aluno/primeiro.c
+++ aluno/primeiro.c
@@ -1,5 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
     printf("primeiro programa!\n");
+    
+    // Este comando faz o Windows aguardar uma tecla antes de fechar o console
+    system("pause"); 
+    
+    return 0;
 }
